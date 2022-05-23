cdaf7e816c82b12f5db0d097ec551ccf48da085d
unknown <suhasmendhe304@gmail.com>
2022-05-23 18:59:34 +0530

    firstcommit

---
+++ src/test/java/com/Trekking/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Trekking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
